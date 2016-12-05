5a7eae418ed289315e16cc2fa127422272f5f948
Anna Lotko <annaklotko@gmail.com>
2016-12-05 07:02:43 -0700

    updated connection in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,12 +3,14 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/bookshelf_dev'
+    // connection: 'postgres://localhost/bookshelf_dev'
+    connection: process.env.DATABASE_URL
   },
 
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/bookshelf_test'
+    // connection: 'postgres://localhost/bookshelf_test'
+    connection: process.env.DATABASE_URL
   },
 
   production: {}
