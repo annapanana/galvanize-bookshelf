2579bc2a4724937d291ec47994f8a24a606f50c4
Anna Lotko <annaklotko@gmail.com>
2016-12-05 07:07:03 -0700

    removed pool from knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,14 +3,12 @@
 module.exports = {
   development: {
     client: 'pg',
-    pool: { min: 1, max: 7 },
     // connection: 'postgres://localhost/bookshelf_dev'
     connection: process.env.DATABASE_URL
   },
 
   test: {
     client: 'pg',
-    pool: { min: 1, max: 7 },
     // connection: 'postgres://localhost/bookshelf_test'
     connection: process.env.DATABASE_URL
   },
