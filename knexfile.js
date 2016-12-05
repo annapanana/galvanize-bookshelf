cbc84663151a629183d3ef2c15448b3caf4f5145
Anna Lotko <annaklotko@gmail.com>
2016-12-05 07:04:58 -0700

    added pool to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,12 +3,14 @@
 module.exports = {
   development: {
     client: 'pg',
+    pool: { min: 1, max: 7 },
     // connection: 'postgres://localhost/bookshelf_dev'
     connection: process.env.DATABASE_URL
   },
 
   test: {
     client: 'pg',
+    pool: { min: 1, max: 7 },
     // connection: 'postgres://localhost/bookshelf_test'
     connection: process.env.DATABASE_URL
   },
